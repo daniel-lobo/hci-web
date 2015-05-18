2e417e9d2e7428fc0ec315c78a2d145ad851241c
Santiago Lezica <slezica89@gmail.com>
2015-05-18 05:33:29 -0300

    app/

--- app/js/controllers/signup.js
+++ app/js/controllers/signup.js
@@ -1,3 +1,31 @@
+app.controller('SignupFormCtrl', function($scope, api) {
+  $scope.form    = {};
+  $scope.loading = false;
+
+  $scope.submit = function() {
+    $scope.loading = true;
+
+    profile = {
+      username    : $scope.form.email,
+      password    : $scope.form.password,
+      firstName   : $scope.form.name,
+      lastName    : "Von Blitzcrank",
+      gender      : 'F',
+      identityCard: $scope.form.number,
+      email       : $scope.form.email,
+      birthDate   : "1970-01-02"
+    }
+
+    console.log('before', profile)
+
+    api.user.signup(profile)
+      .then(function() { delete $scope.error; })
+      .catchSet($scope, 'error')
+      .finally(function(){ $scope.loading = false })
+    ;
+  }
+});
+
 app.directive('signupForm', function () {
   return {
     restrict   : 'A',
@@ -5,16 +33,5 @@ app.directive('signupForm', function () {
     // scope   : true,
     templateUrl: '/views/partials/signup.html',
     controller : 'SignupFormCtrl'
-  }
-});
-
-app.controller('SignupFormCtrl', function($scope) {
-  $scope.signup = function() {
-    $scope.loading = true;
-
-    api.user.signup($scope.form)
-      .catchSet($scope, 'errors')
-      .finally(function(){ $scope.loading = false })
-    ;
   }
 });
