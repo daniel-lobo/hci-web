d8ea2616f92a0fe119b75ba8ea6b04ddca475d00
Santiago Lezica <slezica89@gmail.com>
2015-05-19 22:36:31 -0300

    Corrected messages

--- app/js/services/messages.js
+++ app/js/services/messages.js
@@ -1,20 +1,19 @@
 
 app.factory('m', function() {
   var m = {
-    generic          : "Ha ocurrido un error interno. Disculpe.",
-    invalid_password : "La contraseña debe tener entre 8 y 15 caracteres alfanuméricos.",
-    invalid_email    : "El e-mail ingresado no es válido. No use uno de más de 15 caracteres.",
-    rejected_email   : "El e-mail ingresado no es válido.",
-    rejected_password: "La contraseña ingresada no es válida.",
-    rejected_login   : "Las credenciales ingresadas son incorrectas.",
+    generic          : "Ha ocurrido un error interno. Disculpe",
+    invalid_password : "La contraseña debe tener entre 8 y 15 caracteres alfanuméricos",
+    invalid_email    : "El e-mail ingresado no es válido. Use uno que tenga entre 6 y 15 caracteres",
+    rejected_login   : "Las credenciales ingresadas son incorrectas",
   }
 
   var byCode = {
     001: m.generic,
-    002: m.invalid_email,
-    003: m.invalid_password,
+    002: m.generic,
+    003: m.generic,
     100: m.generic,
     101: m.rejected_login,
+    103: m.generic,
     104: m.invalid_email,
     105: m.invalid_password,
   }
